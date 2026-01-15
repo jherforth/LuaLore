26d69c0de38ce7cdab1d9e2c64cab0b3c657495f
Jay <herforth.jason@gmail.com>
2026-01-15 09:01:04 -0500

    Update arcticblocks.lua

--- arcticblocks.lua
+++ arcticblocks.lua
@@ -32,37 +32,3 @@ minetest.register_craft({
 	output = "default:bronzeblock",
 	recipe = "lualore:sledge",
 })
-
-minetest.register_node("lualore:blanket", {
-    description = S"Blanket",
-    visual_scale = 1,
-    mesh = "Blanket.b3d",
-    tiles = {"textureblanket.png"},
-    inventory_image = "ablanket.png",
-    paramtype = "light",
-    paramtype2 = "facedir",
-    groups = {choppy = 3},
-    drawtype = "mesh",
-    collision_box = {
-        type = "fixed",
-        fixed = {
-            {-0.5, -0.5, -0.5, 0.5, -0.45, 0.5},
-            --[[{-0.5, -0.5, -0.5, 0.5, -0.45, 0.5},
-            {-0.5, -0.5, -0.5, 0.5, -0.45, 0.5}]]
-        }
-    },
-    selection_box = {
-        type = "fixed",
-        fixed = {
-            {-0.5, -0.5, -0.5, 0.5, -0.45, 0.5}
-        }
-    },
-    sounds = default.node_sound_wood_defaults()
-})
-
-minetest.register_craft({
-	type = "cooking",
-	output = "mobs:leather",
-	recipe = "lualore:blanket",
-
-})
